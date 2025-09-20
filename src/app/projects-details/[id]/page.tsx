c132a6f9d6e926da343f8bd278fc8b18e7963d8b
tahmid-khan-hub <tahmidkhan0011@gmail.com>
2025-09-20 22:52:59 +0600

    added projects json data and loaded it in projects-details page

--- src/app/projects-details/[id]/page.tsx
+++ src/app/projects-details/[id]/page.tsx
@@ -1,4 +1,9 @@
-export default async function ProjectDetails() {
+export default async function ProjectDetails({params}: { params: { id: string } }) {
+    const res = await fetch("http://localhost:3000/projectsData.json");
+    const data = await res.json();
+
+    const project = data.find((p) => p.id === params.id);
+    console.log(project);
     return <div>
         <h1>details</h1>
     </div>
