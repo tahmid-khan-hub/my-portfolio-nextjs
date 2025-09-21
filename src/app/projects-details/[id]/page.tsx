ab1a2fc8aaf35a616e5ded98a8a1fc5c72156ad3
tahmid-khan-hub <tahmidkhan0011@gmail.com>
2025-09-21 19:18:11 +0600

    updated projects details page

--- src/app/projects-details/[id]/page.tsx
+++ src/app/projects-details/[id]/page.tsx
@@ -14,51 +14,52 @@ export default async function ProjectDetails({params}: { params: { id: string } 
             <Link href={'/'}><button className="btn"><FaArrowLeftLong size={25} /></button></Link>
 
             {/* content */}
-            <div className="border mt-9 ">
+            <div className="mt-9 mb-5 pt-4 bg-[#0c1220] rounded-xl">
                 {/* title */}
                 <h1 className="text-center text-3xl font-bold mb-5 mt-3">{project.title}</h1>
                 {/* image */}
                 <div>
-                    <Image
+                <Image
                     src={project.image}
                     alt="project image"
                     width={1500}
                     height={1200}
-                    ></Image>
+                    className="w-full h-auto px-2"
+                    />
                 </div>
                 {/* description */}
-                <div className="pl-2">
+                <div className="pl-4">
                 <h5 className="text-xl font-semibold mt-7 mb-2">Description</h5>
                 <p>{project.description}</p>
                 </div>
                 {/* features */}
-                <div className="pl-2">
-                    <h5 className="text-xl font-semibold mt-7 mb-2">Features</h5>
-                    {project.features.map((f, i) => {
-                        <li key={i} className="">{f}</li>
+                <div className="pl-9">
+                    <h5 className="text-xl font-semibold mt-7 mb-2 -ml-4">Features</h5>
+                    {project.features.map((f: string, i: number) => {
+                        return <li key={i} className="">{f}</li>
                     })}
                 </div>
                 {/* tech stack */}
-                <div className="pl-2">
+                <div className="pl-4">
                     <h5 className="text-xl font-semibold mt-7 mb-2">Tech Stack</h5>
-                    {project.technologies.map((tech, i) => {
-                        <button key={i} className="px-3 py-1 text-xs bg-blue-600/20 text-blue-400 rounded-full hover:bg-blue-600/80 hover:text-blue-200">
-                    {tech}
-                  </button>
+                    {project.technologies.map((tech: string, i: number) => {
+                        return <button key={i} className="px-3 py-1 mr-2 my-2 md:my-1 lg:my-0 text-xs bg-blue-600/20 text-blue-400 rounded-full hover:bg-blue-600/80 hover:text-blue-200">
+                        {tech}
+                        </button>
                     })}
                 </div>
                 {/* challenges */}
-                <div className="pl-2">
+                <div className="pl-4">
                     <h5 className="text-xl font-semibold mt-7 mb-2">Challenegs I faced</h5>
                     <p>{project.challenges}</p>
                 </div>
                 {/* learned */}
-                <div className="pl-2">
+                <div className="pl-4">
                     <h5 className="text-xl font-semibold mt-7 mb-2">What I learned</h5>
                     <p>{project.whatILearned}</p>
                 </div>
                 {/* future plan */}
-                <div className="pb-5">
+                <div className="pb-7 pl-4">
                     <h5 className="text-xl font-semibold mt-7 mb-2">Future Plans</h5>
                     <p>{project.futurePlans}</p>
                 </div>
