ad4c13ac18ad98080efb756f5adea629d2724b91
tahmid-khan-hub <tahmidkhan0011@gmail.com>
2026-03-13 21:57:08 +0600

    Created a Navbar folder and changed the navbar import of root layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import "./globals.css";
-import Navbar from "@/components/Navbar";
+import Navbar from "@/components/Navbar/Navbar";
 
 const poppins = Poppins({
   subsets: ["latin"],
