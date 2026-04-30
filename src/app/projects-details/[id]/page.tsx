73c7ef6f8852cb46528efeda9eaf037209789971
tahmid-khan-hub <tahmidkhan0011@gmail.com>
2026-04-30 23:45:22 +0600

    Update the project details page structure

--- src/app/projects-details/[id]/page.tsx
+++ src/app/projects-details/[id]/page.tsx
@@ -5,6 +5,7 @@ import Link from "next/link";
 import { FaArrowLeftLong } from "react-icons/fa6";
 import { ProjectData } from "../ProjectData";
 import AnimateOnView from "@/hooks/AnimateOnView";
+import { FaLongArrowAltLeft } from "react-icons/fa";
 
 export const metadata: Metadata = {
   title: "Portfolio | Project-Details",
@@ -18,13 +19,12 @@ export default async function ProjectDetails({ params, }: { params: Promise<{ id
 
     return <div className="bg-[#0c1220] py-10 min-h-screen">
         <AnimateOnView direction="up" delay={0.25} duration={0.6}><div className="max-w-[1350px] mx-auto px-4 md:px-3 ">
-
             {/* content */}
-            <div className="mt-9 pb-5 pt-4 bg-[#162036] rounded-xl">
+            <div className="mt-9 pb-5 pt-4 rounded-xl">
                 {/* title */}
-                <h1 className="text-center text-3xl text-gray-300 font-bold mb-5 mt-3">{project.title}</h1>
+                <Link href={"/"}><h1 className="flex text-left text-3xl text-gray-300 font-bold mb-10"><FaLongArrowAltLeft className="mr-3 mt-0.5" size={35} />Project Details</h1></Link>
                 {/* image */}
-                <div>
+                <div className="border border-gray-500">
                 <Image
                     src={project.image}
                     alt="project image"
@@ -34,19 +34,19 @@ export default async function ProjectDetails({ params, }: { params: Promise<{ id
                     />
                 </div>
                 {/* description */}
-                <div className="pl-4">
-                <h5 className="text-xl text-gray-300 font-semibold mt-7 mb-2">Description</h5>
-                <p className="text-gray-400">{project.description}</p>
+                <div className="">
+                    <h5 className="text-xl text-gray-300 font-semibold mt-7 mb-2">Description</h5>
+                    <p className="text-gray-400">{project.description}</p>
                 </div>
                 {/* features */}
-                <div className="pl-9">
+                <div className="pl-4.5">
                     <h5 className="text-xl text-gray-300 font-semibold mt-7 mb-2 -ml-4">Features</h5>
                     {project.features.map((f: string, i: number) => {
                         return <li key={i} className="text-gray-400">{f}</li>
                     })}
                 </div>
                 {/* tech stack */}
-                <div className="pl-4">
+                <div className="">
                     <h5 className="text-xl text-gray-300 font-semibold mt-7 mb-2">Tech Stack</h5>
                     {project.technologies.map((tech: string, i: number) => {
                         return <button key={i} className="px-3 py-1 mr-2 my-2 md:my-1 lg:my-0 text-xs bg-blue-600/20 text-blue-400 rounded-full hover:bg-blue-600/80 hover:text-blue-200">
@@ -55,17 +55,17 @@ export default async function ProjectDetails({ params, }: { params: Promise<{ id
                     })}
                 </div>
                 {/* challenges */}
-                <div className="pl-4">
+                <div className="">
                     <h5 className="text-xl text-gray-300 font-semibold mt-7 mb-2">Challenegs I faced</h5>
                     <p className="text-gray-400">{project.challenges}</p>
                 </div>
                 {/* learned */}
-                <div className="pl-4">
+                <div className="">
                     <h5 className="text-xl text-gray-300 font-semibold mt-7 mb-2">What I learned</h5>
                     <p className="text-gray-400">{project.whatILearned}</p>
                 </div>
                 {/* future plan */}
-                <div className="pb-7 pl-4">
+                <div className="pb-7 ">
                     <h5 className="text-xl text-gray-300 font-semibold mt-7 mb-2">Future Plans</h5>
                     <p className="text-gray-400">{project.futurePlans}</p>
                 </div>
